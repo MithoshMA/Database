5f21e2319a2e6f567ba523501f97ef7b9f52dec1
Mithosh <ma.mithosh@gmail.com>
2023-07-09 03:00:07 +0900

    updated

--- SQLQuery_3.sql
+++ SQLQuery_3.sql
@@ -25,3 +25,45 @@ drop view UserName
 create view UserName as select ug_id as 'ID', ug_name as 'Name' from TblUserGroup where ug_flag = 0;
 
 select * from  UserName
+
+
+SELECT * FROM TblChitTrans 
+
+SELECT
+L.lot_number as 'Installment',
+A.tct_lot_no as 'Lot_No',
+(select convert(varchar, [L].[lot_date] , 1)) as 'LOT_Date',
+B.ctmbr_mbr_id as 'Memebr_ID',
+C.mem_first_name as 'First_Name',
+C.mem_last_name as 'Last_Name',
+S.SectorName as 'Sector_Name',
+L.lot_pay_amount as 'Inst_Amount',
+A.tct_paid_amount as 'Paid_Amount',
+CAST((L.lot_pay_amount -A.tct_paid_amount) as INT) as 'Balance',
+AGC.agt_status as 'Agent_Name'
+FROM TblChitTrans A
+LEFT JOIN TblChitMemberInfo B ON A.tct_lot_no = B.ctmbr_lot_no
+LEFT JOIN TblMembers C ON B.ctmbr_mbr_id = C.mem_id_no 
+LEFT JOIN TblLotDateInfo L ON A.tct_lot_id = L.lot_id_no
+LEFT JOIN TblSector S ON B.ctmbr_sector = S.sectorId
+LEFT JOIN TblAgents AGC ON A.tct_agent_id = AGC.agt_id
+
+SELECT
+L.lot_number as 'Installment',
+A.tct_lot_no as 'Lot_No',
+(select convert(varchar, [L].[lot_date] , 1)) as 'LOT_Date',
+B.ctmbr_mbr_id as 'Memebr_ID',
+C.mem_first_name as 'First_Name',
+C.mem_last_name as 'Last_Name',
+S.SectorName as 'Sector_Name',
+L.lot_pay_amount as 'Inst_Amount',
+A.tct_paid_amount as 'Paid_Amount',
+CAST((L.lot_pay_amount -A.tct_paid_amount) as INT) as 'Balance',
+A.tct_agent_id as 'Agent',
+AGC.mem_first_name as 'Agent_Name'
+FROM TblChitTrans A
+LEFT JOIN TblChitMemberInfo B ON A.tct_lot_no = B.ctmbr_lot_no
+LEFT JOIN TblMembers C ON B.ctmbr_mbr_id = C.mem_id_no 
+LEFT JOIN TblLotDateInfo L ON A.tct_lot_id = L.lot_id_no
+LEFT JOIN TblSector S ON B.ctmbr_sector = S.sectorId
+LEFT JOIN TblMembers AGC ON tct_agent_id = AGC.mem_id_no
--- Tables/TblAgent.sql
+++ Tables/TblAgent.sql
@@ -1,8 +1,10 @@
+
 CREATE TABLE TblAgents
 (
     agt_id INT IDENTITY,
     agt_sectorId   VARCHAR(50),
-    agt_memb_id   INT,
+    agt_first_name   VARCHAR(50),
+    agt_last_name   VARCHAR(50),
     agt_status INT DEFAULT 0
     FOREIGN KEY (agt_sectorId) REFERENCES  TblSector(sectorId),
     FOREIGN KEY (agt_memb_id) REFERENCES  TblMembers(mem_id_no),
@@ -22,5 +24,46 @@ VALUES
 SELECT M.mem_first_name + ' ' + mem_last_name,
 S.SectorName FROM
 TblAgents T 
-LEFT JOIN TblMembers M ON T.agt_memb_id = M.mem_id_no
-LEFT JOIN TblSector S ON T.agt_sectorId = S.sectorId
+JOIN TblMembers M ON T.agt_memb_id = M.mem_id_no
+JOIN TblSector S ON T.agt_sectorId = S.sectorId
+
+GO
+DROP TABLE TblAgent
+GO
+CREATE TABLE TblAgent
+(
+    agt_id INT IDENTITY,
+    agt_no INT,
+    agt_sectorId   VARCHAR(50),
+    agt_first_name   NVARCHAR(50),
+    agt_last_name   NVARCHAR(50),
+    agt_status INT DEFAULT 0
+    FOREIGN KEY (agt_sectorId) REFERENCES  TblSector(sectorId),
+);
+GO
+
+GO
+DROP VIEW RPT_VW_AGENTS
+GO
+CREATE VIEW RPT_VW_AGENTS as
+SELECT 
+A.agt_id,
+A.agt_no as 'Agent No',
+A.agt_first_name as 'First_Name',
+A.agt_last_name as 'Last_Name',
+S.SectorName as 'Sector'
+FROM TblAgent A
+LEFT JOIN TblSector S ON A.agt_sectorId = S.sectorId
+GO
+
+SELECT * FROM TblAgent
+
+INSERT INTO TblAgent
+(agt_sectorId, agt_first_name, agt_last_name)
+VALUES ('TPR_001', 'Shimnesh', 'TP'),
+ ('AKL_002', 'Sunil Kumar', 'A'),
+  ('AKL_001', 'Sunil Kumar', 'A'),
+ ( 'NYK_001', 'Abas', 'NK'),
+ ( 'KAK_001', 'Gireesh', 'KK')
+
+
